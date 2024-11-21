2a8594a11ee7433a9f77ca5b259aebf73f40f06e
rKim19300 <reececuylerkim@gmail.com>
2024-11-21 01:14:57 -0800

    Added Springboot backend

---
+++ backend/src/test/java/com/cs157a/studentmanagement/StudentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs157a.studentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
